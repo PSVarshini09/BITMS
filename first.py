2a06d737f064b6c33cede37b0719c875b6466dde
PSVarshini09 <167341962+PSVarshini09@users.noreply.github.com>
2024-04-26 21:38:28 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,22 @@
+a=5
+a=a<<3
+print(a)
+a=25
+a=a<<5
+print(a)
+a=25
+a=a>>2
+print(a)
+print(a>-10 and a>20 and a<10)
+print(a>10 or a>2 )
+n='varshini'
+print('i' in n)
+a=(1,5,8)
+print(6 not in a)
+a=5
+b=5
+print(a is b)
+a=5
+b=5.0
+print(a is b)
+print(a is not b)
